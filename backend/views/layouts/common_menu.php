6c7004f2da3c777d27059b11a503cfc9b7fb92b3
bfyang5130 <bfyang5130@qq.com>
2017-10-25 09:46:27 +0800

    初始化布局格式样例

--- backend/views/layouts/common_menu.php
+++ backend/views/layouts/common_menu.php
@@ -1,9 +1,12 @@
+<?php
+use yii\helpers\Url;
+?>
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> 我的站点</a>
     <ul>
         <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>我的站点</span></a></li>
         <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>我的布局</span>　</a>
             <ul>
-                <li><a href="form-common.html">站点布局</a></li>
+                <li><a href="<?= Url::toRoute(['/plot/index']) ?>">站点布局</a></li>
                 <li><a href="form-validation.html">已购布局</a></li>
                 <li><a href="form-wizard.html">布局商城</a></li>
             </ul>
