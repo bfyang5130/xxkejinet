201c4f2d4fe82d20297eea12ec61267a940a48f0
bfyang5130 <bfyang5130@qq.com>
2017-11-25 00:17:06 +0800

    fit login layout

--- backend/views/login/index.php
+++ backend/views/login/index.php
@@ -14,21 +14,31 @@ $this->title = Yii::$app->params['webname'];
             </div>
         </div>
         <div class="am-u-sm-10 login-am-center">
-            <p class="login-box-msg text-red"><?php echo current($model->getFirstErrors()) ?></p>
-            <?php $form = ActiveForm::begin(['id' => 'loginform', 'class' => 'am-form', 'action' => Url::toRoute(['/login/index'])]); ?>
+            <?php $form = ActiveForm::begin(['id' => 'loginform', 'options' => ['class' => 'am-form'], 'action' => Url::toRoute(['/login/index'])]); ?>
             <fieldset>
                 <?=
-                        $form->field($model, 'username', ["template" => '<div class="am-form-group">{input}</div>'])
+                        $form->field($model, 'username', ['options' => ['class' => 'am-form-group'], "template" => '{input}'])
                         ->textInput(["placeholder" => "帐号"]);
                 ?>
                 <?=
-                        $form->field($model, 'password', ["template" => '<div class="am-form-group">{input}</div>'])
-                        ->passwordInput(["placeholder" => "密码"]);
+                        $form->field($model, 'password', ['options' => ['class' => 'am-form-group'], "template" => '{input}'])
+                        ->passwordInput(["placeholder" => "密码",'style'=>'border-radius: 0px 0px 0px 0px;']);
                 ?>
                 <?=
-                $form->field($model, 'verifyCode')->widget(Captcha::className(), ['captchaAction' => 'login/captcha',
-                    'template' => '<div class="am-form-group">{image}{input}</div>', 'imageOptions' => ['width' => '100px']
-                ])
+                        $form->field($model, 'verifyCode', ['options' => ['class' => 'am-form-group'], 'template' => Captcha::widget(
+                                    [
+                                        'model'=>$model,
+                                        'attribute'=>'verifyCode',
+                                        'captchaAction' => 'login/captcha',
+                                        'template' => '{image}',
+                                        'imageOptions' =>
+                                        [
+                                            'alt' => '点击换图',
+                                            'title' => '点击换图',
+                                            'style' => 'border-radius: 0px 0px 0px 6px;margin-top: 0px;float: left;height: 30px;cursor:pointer;width:25%;']
+                                    ]
+                    ).'{input}'])
+                        ->textInput(['placeholder' => '验证码', 'style' => 'border-radius: 0px 0px 6px 0px;width: 75%;'])
                 ?>
                 <p><button type="submit" class="am-btn am-btn-default">登录</button></p>
             </fieldset>
